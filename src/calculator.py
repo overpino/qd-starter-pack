31aa1838a4dfbc2f6e1042b0f1a24e40b5d15a10
Salvatore Fede <salvofede1234@outlook.it>
2025-11-25 22:16:07 +0100

    feat: nuova implementazione della calcolatrice, unit-test annesso

--- src/calculator.py
+++ src/calculator.py
@@ -11,15 +11,35 @@
 #   Division: 2
 #
 
-def operazioni(num1:int, num2: int) -> None:
-    print("Somma:", num1 + num2)
-    print("Differenza:", num1 - num2)
-    print("Prodotto:", num1 * num2)
-    if num2 == 0:
-        print("Divisione non possibile. Il divisore è 0.")
-    else:
-        print("Divisione:", num1 / num2)
 
-a = int(input("Inserisci il primo numero: "))
-b = int(input("Inserisci il secondo numero: "))
-operazioni(a, b)
+def addizione(num1: float, num2: float) -> float:
+    return num1 + num2
+
+
+def sottrazione(num1: float, num2: float) -> float:
+    return num1 - num2
+
+
+def moltiplicazione(num1: float, num2: float) -> float:
+    return num1 * num2
+
+
+def divisione(num1: float, num2: float) -> float | str:
+    if num2 == 0:
+        return "incalcolabile. Divisione per zero non consentita."
+    return num1 / num2
+
+
+def prendi_input() -> tuple[float, float]:
+    num1 = float(input("Inserisci il primo numero: "))
+    num2 = float(input("Inserisci il secondo numero: "))
+    return num1, num2
+
+
+if __name__ == "__main__":
+    a, b = prendi_input()
+
+    print("La somma è", addizione(a, b))
+    print("La differenza è", sottrazione(a, b))
+    print("Il prodotto è", moltiplicazione(a, b))
+    print("Il quoziente è", divisione(a, b))
