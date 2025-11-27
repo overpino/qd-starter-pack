ccaefc2c536761bf62d235cabf385de5ce2d34cc
Salvatore Fede <salvofede1234@outlook.it>
2025-11-27 21:08:57 +0100

    test: aggiunto unit-test, leggere migliorie al codice originale

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -6,45 +6,52 @@
 #   The binary number is: 1000
 #
 
-def division(a: int) -> int:
-    b = ''
+
+def division(a: int) -> str:
+    b = ""
     while a > 0:
         if a % 2 == 0:
-            b = '0' + b
+            b = "0" + b
         else:
-            b = '1' + b
+            b = "1" + b
         a = a // 2
-    return b
+    return f"{b}"
 
-def carry(a: int) -> int:
-    result_list = list(a) # conversione dell'array in lista
 
-    for i in range(len(result_list) - 1, -1, -1): # gestione del riporto di 1
-        if result_list[i] == '0':
-            result_list[i] = '1'
+def carry(a: str) -> str:
+    result_list = list(a)  # conversione dell'array in lista
+
+    for i in range(len(result_list) - 1, -1, -1):  # gestione del riporto di 1
+        if result_list[i] == "0":
+            result_list[i] = "1"
             break
-    result_list[i] = '0'
+        result_list[i] = "0"
 
     a = "".join(result_list)
     return a
 
-result = ''
-input = int(input("Inserisci il numero da convertire: "))
 
-if input == 0:
-    print("0")
+def invert(res: str) -> str:
+    res = res.zfill(16)
+    res = res.replace("0", "&").replace("1", "0").replace("&", "1")
+    return res
 
-elif input < 0:
-    input = -input
-    result = division(input)
 
-    result = result.zfill(16)
-    result = result.replace('0', '&').replace('1', '0').replace('&', '1')
+if __name__ == "__main__":
+    result = ""
+    input = int(input("Inserisci il numero da convertire: "))
 
-    print(f"Stringa modificata: {carry(result)}")
+    if input == 0:
+        print("0")
 
-    print("Il seguente numero è calcolato con la tecnica del complemento a due.")
-    print("Il risultato è rappresentato in 16 bit.")
+    elif input < 0:
+        input = -input
+        result = division(input)
+        result = invert(result)
 
-elif input >= 0:
-    print(division(input))
+        print(f"Stringa modificata: {carry(result)}")
+        print("Il seguente numero è calcolato con la tecnica del complemento a due.")
+        print("Il risultato è rappresentato in 16 bit.")
+
+    elif input >= 0:
+        print(division(input))
